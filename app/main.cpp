959055ffa3eccd1d8fdf3d70a3daa86ae7aa88e9
Chin-Po <aupt23@gmail.com>
2018-10-14 20:42:28 -0400

    Task 4.6 (AI6): Implement main funciton
    - build perception object to handle the human detection function
    - read all the images in imageData/Data

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
+#include "perception.h"
+#include "inputHandle.h"
+#include "imageProcess.h"
+#include "hogHumanDetect.h"
+#include "outputDisplay.h"
+#include <fstream>
+#include <sstream>
+#include <dirent.h>
 
-int main()
-{
-    std::cout << "hello world" << std::endl;
-    return 0;
+int main() {
+	perception perceptionObject;
+	DIR *pDIR;
+	std::vector<std::string> files;
+	struct dirent *entry;
+	if( pDIR = opendir("../imageData/Data") ){
+		while(entry = readdir(pDIR)){
+			if( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 ){
+				std::string imageName = entry->d_name;
+				imageName = "../imageData/Data/" + imageName;
+				perceptionObject.run(imageName);
+			}
+		}
+		closedir(pDIR);
+	}
+	else{
+		std::cout << "CANNOT OPEN!!!" << std::endl;
+	}
+	return 0;
 }
+
