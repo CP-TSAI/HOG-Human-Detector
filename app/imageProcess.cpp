6b14468196a7202bc95e139fb700d2badf247b3f
Chin-Po <aupt23@gmail.com>
2018-10-14 20:12:15 -0400

    Task 4.3 (AI3): Implement class imageProcess
    - implement imageProcess() with basic OpenCV tools: resize(), histogramEqualization(), toGray(), lowPassFilter()

--- app/imageProcess.cpp
+++ app/imageProcess.cpp
@@ -2,17 +2,21 @@
 #include "imageProcess.h"
 
 cv::Mat imageProcess::resizeImage(cv::Mat& img){
+	cv::resize(img, img, cv::Size(640, 480));
 	return img;
 }
 
 cv::Mat imageProcess::histogramEqualization(cv::Mat& img){
+	equalizeHist(img, img);
 	return img;
 }
 
 cv::Mat imageProcess::toGray(cv::Mat& img){
+	cvtColor(img, img, CV_BGR2GRAY);
 	return img;
 }
 
 cv::Mat imageProcess::lowPassFilter(cv::Mat& img){
+	blur(img, img, Size(3, 3));
 	return img;
 }
