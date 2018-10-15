25ab1ec4629afff8107b504749b4eb02a4be857f
Chin-Po <aupt23@gmail.com>
2018-10-14 20:06:07 -0400

    Task 4.2 (AI2): Implement class inputHandle
    - implement readImage() with imread and check if read successfully

--- app/inputHandle.cpp
+++ app/inputHandle.cpp
@@ -1,9 +1,12 @@
-
 #include "inputHandle.h"
-#include <string>
 
-// ---------- inputHandle
 cv::Mat inputHandle::readImage(std::string imgName){
-	cv::Mat image;
+	image = cv::imread(imgName , CV_LOAD_IMAGE_COLOR);
+
+	if(! image.data ) {
+		std::cout << errorMessage << std::endl;
+		return cv::Mat::zeros(cvSize(2, 2), CV_8UC3);
+	}
+	isReadSuccessful = 1;
 	return image;
 }
